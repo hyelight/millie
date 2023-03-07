1662bfcf93d757146a4a04e16196b38d83dd6e86
hyelight <zheldeld@gmail.com>
2023-03-07 19:00:14 +0900

    books section 정적 json 데이터 생성&순회, books section 관련 모든 css

--- js/main.js
+++ js/main.js
@@ -1,9 +1,66 @@
-const playButton = document.querySelector(".play-button");
-const thumbnailImage = document.querySelector(".thumbnail-image");
-const videoIframe = document.querySelector(".video-iframe");
+/* ----- books section 정적 데이터 순회 ----- */
+// bestseller section
+fetch("../JSON/books.json")
+  .then((res) => res.json())
+  .then((data) => {
+    data.bestseller.forEach((book, i) => {
+      const eachBook = document.querySelectorAll(".each-book-each")[i];
+      eachBook.style.backgroundImage = `url(${book.image})`;
 
-playButton.addEventListener("click", () => {
-  thumbnailImage.style.display = "none";
-  playButton.style.display = "none";
-  videoIframe.src = "https://www.youtube.com/embed/u1OVbU4EdwI?autoplay=1";
-});
+      const eachInfo = document.querySelectorAll(".a-book-info")[i];
+      eachInfo.innerHTML = `
+        <p class="book-title">${book.title}</p>
+        <p class="book-author">${book.author}</p>
+        <div class="complete-percent-time-wrapper">
+          <img src="img/icon-complete-reading.png" alt="percent와 time 정보" />
+          <span class="book-percent-time">${book.percent} &nbsp;| &nbsp;${book.time}</span>
+        </div>
+      `;
+    });
+
+    // month section
+    data.month.forEach((book, i) => {
+      const eachBook = document.querySelectorAll(".each-book-each-month")[i];
+      eachBook.style.backgroundImage = `url(${book.image})`;
+
+      const eachInfo = document.querySelectorAll(".a-book-info-month")[i];
+      eachInfo.innerHTML = `
+        <p class="book-title">${book.title}</p>
+        <p class="book-author">${book.author}</p>
+        <div class="complete-percent-time-wrapper">
+          <img src="img/icon-complete-reading.png" alt="percent와 time 정보" />
+          <span class="book-percent-time">${book.percent} &nbsp;| &nbsp;${book.time}</span>
+        </div>
+      `;
+    });
+
+    // audiobest section
+    data.audiobest.forEach((book, i) => {
+      const eachBook = document.querySelectorAll(".each-book-each-audiobest")[
+        i
+      ];
+      eachBook.style.backgroundImage = `url(${book.image})`;
+
+      const eachInfo = document.querySelectorAll(".a-book-info-audiobook")[i];
+      eachInfo.innerHTML = `
+        <p class="book-title">${book.title}</p>
+        <p class="book-author">${book.author}</p>
+        <div class="complete-percent-time-wrapper">
+          <img src="img/icon-complete-reading.png" alt="percent와 time 정보" />
+          <span class="book-percent-time">${book.percent} &nbsp;| &nbsp;${book.time}</span>
+        </div>
+      `;
+    });
+
+    // original section
+    data.original.forEach((book, i) => {
+      const eachBook = document.querySelectorAll(".each-book-each-original")[i];
+      eachBook.style.backgroundImage = `url(${book.image})`;
+
+      const eachInfo = document.querySelectorAll(".a-book-info-original")[i];
+      eachInfo.innerHTML = `
+        <p class="book-title">${book.title}</p>
+        <p class="book-author">${book.author}</p>
+      `;
+    });
+  });
